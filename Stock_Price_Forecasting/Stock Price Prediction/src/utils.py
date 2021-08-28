e01c4a57e7ce0490ba7bc1ca2678f06d61b4fc1f
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-28 16:30:58 +0545

    Complete the all process of feature Engineering

--- Stock_Price_Forecasting/Stock Price Prediction/src/utils.py
+++ Stock_Price_Forecasting/Stock Price Prediction/src/utils.py
@@ -18,23 +18,29 @@ class FeatureEngineering:
         return(scaler.fit_transform(np.array(self.dataframe).reshape(-1,1)))
     
     @classmethod
-    def split_dataset(self, split_size : float = 0.8, n_lag : int = 5):
+    def split_dataset(self, split_size : float = 0.8, n_lag : int = 1, reshape3D : bool = True):
         '''
+        reshape input to [samples, time steps, features]
         '''
         data_set = scale_dataset()
-
+        #split the data into trainset, validationset, testset
         length_trainingdata = math.ceil(len(data_set) * split_size)
         length_trainset = math.ceil(length_trainingdata * split_size)
         train_set = data_set[: length_trainset, :]
         validation_set = data_set[length_trainset:, length_trainingdata, :]
         test_set = data_set[length_trainingdata :, :]
-    
+        #split the different dataset into 
         X_train, Y_train = train_set[:, :n_lag * self.dataframe.shape[1]], train_set[:, -self.dataframe.shape[1]]
         X_validation, Y_validation = validation_set[:, :n_lag * self.dataframe.shape[1]], validation_set[:,-self.dataframe.shape[1]]
         X_test, Y_test = test_set[:, :n_lag * self.dataframe.shape[1]], test_set[:, -self.dataframe.shape[1]]
+        #Reshape the data X value into 3D
+        if reshape3D:
+            X_train = np.reshape(X_train, (X_train.shape[0], n_lag, self.dataframe.shape[1]))
+            X_validation = np.reshape(X_validation, (X_validation.shape[0], n_lag, self.dataframe.shape[1]))
+            X_test = np.reshape(X_test, (X_test.shape[0], n_lag, self.dataframe.shape[1]))
 
         return X_train, X_validation, X_test, Y_train, Y_validation, Y_test
-
+    
     def series_to_supervised(data : array, n_lag : int = 5 , num_label : int  = 1, dropnan : bool = True) -> Dataframe:
         '''
         '''
@@ -60,3 +66,6 @@ class FeatureEngineering:
             new_dataframe.dropna(inplace = True)
 
         return new_dataframe
+    
+
+    
