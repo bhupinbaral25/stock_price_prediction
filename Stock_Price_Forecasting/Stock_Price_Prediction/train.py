8077b090bd043e09ac4059533010f002ae78548f
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-30 22:32:09 +0545

    Model initialize

--- Stock_Price_Forecasting/Stock_Price_Prediction/train.py
+++ Stock_Price_Forecasting/Stock_Price_Prediction/train.py
@@ -6,19 +6,22 @@ import numpy as np
 
 
 def built_model(X_train: np.ndarray, units: int = 120, dropout: int = 0.2):
-    """ """
+    """
+    This  functions initialize the model with neccessary parameters X_train shaped
+    is used to identify the input shape of the model.
+    """
     model = Sequential()
     model.add(
         LSTM(
-            units=units,
-            return_sequences=True,
-            dropout=dropout,
-            input_shape=(X_train.shape[1], X_train.shape[2]),
+            units = units,
+            return_sequences = True,
+            dropout = dropout,
+            input_shape = (X_train.shape[1], X_train.shape[2]),
         )
     )
-    model.add(LSTM(units=units, return_sequences=True))
-    model.add(LSTM(units=units))
+    model.add(LSTM(units = units, return_sequences = True))
+    model.add(LSTM(units = units))
     model.add(Dense(1))
-    model.compile(loss="mse", optimizer="adam", metrics=["mse", "mae"])
+    model.compile(loss = "mse", optimizer = "adam", metrics = ["mse", "mae"])
 
     return model
