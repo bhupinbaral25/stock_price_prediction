e5d63fe299f72e07661baaeaef7e8f93a3cbd902
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-31 14:48:05 +0545

    Deleted Readme

--- Stock_Price_Forecasting/views.py
+++ Stock_Price_Forecasting/views.py
@@ -1,12 +1,32 @@
+import keras
+import matplotlib.pyplot as plt
 import os
-import pickle
-import streamlit as st
-import joblib
-from Stock_Price_Prediction.src.utils import split_dataset
+from Stock_Price_Prediction.src.utils import split_dataset, series_to_supervised
 from Scripts import read_data as rd
+import streamlit as st 
 
+
+model = keras.models.load_model('model_checkpoint')
+n_lag = 12
 base = os.path.dirname(os.path.abspath(__file__))
-if __name__ == '__main__':
+path = os.path.join(base, "data/processed/final_dataset.csv")
+dataframe = rd.preprocess_data(rd.read_dataset(path))
+
+'''Spliting the dataset and reshape them into the compatible input for model'''
     
-    pickled_model = pickle.load(open('model_checkpoints/model.pickle', 'rb'))
+X_train, X_validation, X_test, Y_train, Y_validation, Y_test = split_dataset(
+    dataframe, n_lag = n_lag)
+
+st.text('Hello User Here is the forcasting for ADBl ')
+
+predict = model.predict(X_test)
+actual = Y_test
+st.plotly_chart(predict)
+
+
+
+
+
+
+
 
